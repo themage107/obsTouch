48632a82619a9a301ae5a51e7718d65979e60238
Adrian J. Fox <themage107@gmail.com>
2018-11-01 16:59:46 -0700

    Prevent from sticking to areas you shouldn't stick to - alt to physics materials
    Physics Smysics. Use this to prevet the character from sticking to walls
    they shouldn't. Don't need the slippery setting now.

---
+++ obsTouch.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obsTouch : MonoBehaviour {
+
+    public BoxCollider2D thisBox;
+    float xValue;
+
+    void Start()
+    {
+        thisBox = this.gameObject.GetComponent<BoxCollider2D>();
+        xValue = thisBox.transform.position.x;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "wallCheck")
+        {
+            moveJohnnyUpdated obsTouchInstance = collision.gameObject.transform.parent.GetComponent<moveJohnnyUpdated>();
+            obsTouchInstance.obsTouching = true;
+            obsTouchInstance.obsX = xValue;
+        }
+        if (collision.gameObject.name == "JohnnyHitBox")
+        {
+            moveJohnnyUpdated obsTouchInstanceJ = collision.gameObject.transform.parent.GetComponent<moveJohnnyUpdated>();
+            obsTouchInstanceJ.obsTouchingJohnny = true;
+            obsTouchInstanceJ.obsX = xValue;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "wallCheck")
+        {
+            moveJohnnyUpdated obsTouchInstance = collision.gameObject.transform.parent.GetComponent<moveJohnnyUpdated>();
+            obsTouchInstance.obsTouching = false;
+            obsTouchInstance.obsX = 0;
+        }
+        if (collision.gameObject.name == "JohnnyHitBox")
+        {
+            moveJohnnyUpdated obsTouchInstanceJ = collision.gameObject.transform.parent.GetComponent<moveJohnnyUpdated>();
+            obsTouchInstanceJ.obsTouchingJohnny = false;
+            obsTouchInstanceJ.obsX = xValue;
+        }
+    }
+}
